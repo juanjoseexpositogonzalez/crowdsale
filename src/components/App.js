35e2e639ae1e65d3285cbc73b58a0a834430dfbf
Juanjo <expositogonzalez.juanjose@gmail.com>
2025-10-14 15:23:39 +0200

    Add Accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
+
+// Components
 import Navigation from "./Navigation";
+import Info from "./Info";
 
 function App() {
+
+    const [account, setAccount] = useState(null);
+
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        console.log(provider);
+
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0]);
+        console.log(account);
+
+        // Add to state
+        setAccount(account);
+
+    }
+
+    useEffect(() => {
+        loadBlockchainData();
+    })
+
     return (
         <Container>
             <Navigation />
+            <hr />
+            {account && (
+                <Info account={account} />
+            )}
         </Container>
     )
 }
