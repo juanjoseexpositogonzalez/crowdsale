afb23590b0efd5efa9e1be565d63e1a60421dc2c
Juanjo <expositogonzalez.juanjose@gmail.com>
2025-10-14 13:12:29 +0200

    Navigation Bar

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+import Navigation from "./Navigation";
+
+function App() {
+    return (
+        <Container>
+            <Navigation />
+        </Container>
+    )
+}
+
+export default App;
