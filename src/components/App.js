f9c20717e4e52a5e70958d6a85cf7c2c16a302e5
Juanjo <expositogonzalez.juanjose@gmail.com>
2025-10-15 06:48:51 +0200

    Buy Tokens

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 
 // Components
 import Navigation from "./Navigation";
+import Buy from "./Buy";
 import Info from "./Info";
 import Loading from "./Loading";
 import Progress from "./Progress";
@@ -69,7 +70,7 @@ function App() {
         // Add to state
         setIsLoading(false);
 
-    }
+    };
 
     useEffect(() => {
         if (isLoading) {
@@ -87,6 +88,7 @@ function App() {
             ) : (
                 <>
                     <p className='text-center'><strong>Current Price:</strong> {price} ETH</p>
+                    <Buy provider={provider} price={price} crowdsale={crowdsale} setIsLoading={setIsLoading} />
                     <Progress tokensSold={tokensSold} maxTokens={maxTokens} />
                 </>
             )}
